951a2bdeedac6ecf0a0637ab340d3b92307a00ea
Olu Adeyemo:Astronaut <theoafactor@gmail.com>
2024-02-17 18:30:27 +0100

    BUild Docker image

--- _devops/setupweb.sh
+++ _devops/setupweb.sh
@@ -4,6 +4,6 @@ cd /var/www
 sudo chown -R $USER html/
 sudo rm -rf html
 sudo mkdir html
-sudo chown -R $USER html/
-cd html
-sudo git clone https://github.com/theoafactor/SimplePersonal.git .
+# sudo chown -R $USER html/
+# cd html
+# sudo git clone https://github.com/theoafactor/SimplePersonal.git .
