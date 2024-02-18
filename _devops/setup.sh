733361fa9fb7f219ab8ce62f05dc1f9c65b4309b
Olu Adeyemo:Astronaut <theoafactor@gmail.com>
2024-02-18 18:45:39 +0100

    Added Terraform

--- _devops/setup.sh
+++ _devops/setup.sh
@@ -20,16 +20,16 @@ sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin 
 
 
 ## install runner
-mkdir /home/ubuntu/actions-runner 
+# mkdir /home/ubuntu/actions-runner 
 
-cd /home/ubuntu/actions-runner
+# cd /home/ubuntu/actions-runner
 
-curl -o actions-runner-linux-x64-2.311.0.tar.gz -L https://github.com/actions/runner/releases/download/v2.311.0/actions-runner-linux-x64-2.311.0.tar.gz
+# curl -o actions-runner-linux-x64-2.311.0.tar.gz -L https://github.com/actions/runner/releases/download/v2.311.0/actions-runner-linux-x64-2.311.0.tar.gz
 
-echo "29fc8cf2dab4c195bb147384e7e2c94cfd4d4022c793b346a6175435265aa278  actions-runner-linux-x64-2.311.0.tar.gz" | shasum -a 256 -c
+# echo "29fc8cf2dab4c195bb147384e7e2c94cfd4d4022c793b346a6175435265aa278  actions-runner-linux-x64-2.311.0.tar.gz" | shasum -a 256 -c
 
-tar xzf ./actions-runner-linux-x64-2.311.0.tar.gz
+# tar xzf ./actions-runner-linux-x64-2.311.0.tar.gz
 
-echo -ne "\n" | ./config.sh --url https://github.com/theoafactor/SimplePersonal --token ADKQKWML5ABC47J53DKFBSTFMDFGI
+# echo -ne "\n" | ./config.sh --url https://github.com/theoafactor/SimplePersonal --token ADKQKWML5ABC47J53DKFBSTFMDFGI
 
-./run.sh&
+# ./run.sh&
