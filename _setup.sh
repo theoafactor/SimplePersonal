25ac806a94a16274cdd54db4336efb020166b2fa
Olu Adeyemo:Astronaut <theoafactor@gmail.com>
2024-02-25 18:04:12 +0100

    New Deployment 3

--- _setup.sh
+++ _setup.sh
@@ -42,5 +42,12 @@ sudo apt-get update
 sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
 
 
+## build the image and run as container 
+echo "Building and Running Docker Container ..."
+BUILD_TAG=$(echo $RANDOM)
+sudo docker build -t simpleweb.com:$BUILD_TAG .
+sudo docker run -d -p 80:80 simpleweb.com:$BUILD_TAG
+
+
 
 
