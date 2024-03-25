ff7c115397e51e98cba0d1a23084e7dbdb01b7f8
Olu Adeyemo:Astronaut <theoafactor@gmail.com>
2024-03-25 01:36:26 +0100

    Worked on pam.d and modules

--- _setup.sh
+++ _setup.sh
@@ -42,22 +42,85 @@ sudo apt-get update
 
 sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
 
-# echo "Cloning Repository .."
 
-# sudo git clone -b test https://github.com/theoafactor/SimplePersonal.git web
-
-# sudo chown -R $USER web/
-
-# sudo chown -R $USER web
-
-# cd web
-
-# ## build the image and run as container 
-# echo "Building and Running Docker Container ..."
-# BUILD_TAG=$(echo $RANDOM)
-# sudo docker build -t simpleweb.com:$BUILD_TAG .
-# sudo docker run -d -p 80:80 simpleweb.com:$BUILD_TAG
+## Create New Users 
+# - jamesuser 
 
 
+sudo useradd jamesuser
 
+# set the password for jamesuser
+sudo echo "jamesuser:jamesuser" | sudo chpasswd
+
+# create the no group
+sudo groupadd nosu
+
+
+sudo usermod -aG nosu jamesuser
+
+
+sudo chown $USER /etc/pam.d/su 
+
+sudo rm -f /etc/pam.d/su
+
+sudo touch /etc/pam.d/su
+
+sudo chown $USER /etc/pam.d/su
+
+sudo cat > /etc/pam.d/su << EOL
+#
+# The PAM configuration file for the Shadow 'su' service
+#
+# This allows root to su without passwords (normal operation)
+#auth       sufficient pam_rootok.so
+
+# Uncomment this to force users to be a member of group wheel
+# before they can use 'su'. You can also add "group=foo"
+# to the end of this line if you want to use a group other
+# than the default "wheel" (but this may have side effect of
+# denying "root" user, unless she is a member of "foo" or explicitly
+# permitted earlier by e.g. "sufficient pam_rootok.so").
+# (Replaces the 'SU_WHEEL_ONLY' option from login.defs)
+# auth       required   pam_wheel.so
+# Uncomment this if you want wheel members to be able to
+# su without a password.
+# auth       sufficient pam_wheel.so trust
+# Uncomment this if you want members of a specific group to not
+# be allowed to use su at all.54.196.106.38
+auth       required   pam_wheel.so deny group=nosu
+# Uncomment and edit /etc/security/time.conf if you need to set
+# time restrainst on su usage.
+# (Replaces the 'PORTTIME_CHECKS_ENAB' option from login.defs
+# as well as /etc/porttime)
+# account    requisite  pam_time.so
+# This module parses environment configuration file(s)
+# and also allows you to use an extended config
+# file /etc/security/pam_env.conf.
+# 
+# parsing /etc/environment needs "readenv=1"
+session       required   pam_env.so readenv=1
+# locale variables are also kept into /etc/default/locale in etch
+# reading this file *in addition to /etc/environment* does not hurt
+session       required   pam_env.so readenv=1 envfile=/etc/default/locale
+
+# Defines the MAIL environment variable
+# However, userdel also needs MAIL_DIR and MAIL_FILE variables
+# in /etc/login.defs to make sure that removing a user 
+# also removes the user's mail spool file.
+# See comments in /etc/login.defs
+#
+# "nopen" stands to avoid reporting new mail when su'ing to another user
+session    optional   pam_mail.so nopen
+
+# Sets up user limits according to /etc/security/limits.conf
+# (Replaces the use of /etc/limits in old login)
+session    required   pam_limits.so
+
+# The standard Unix authentication modules, used with
+# NIS (man nsswitch) as well as normal /etc/passwd and
+# /etc/shadow entries.
+@include common-auth
+@include common-account
+@include common-session
+EOL
 
---
+++ modules/install_nginx/install.tf
@@ -0,0 +1,95 @@
+// after the initial setup, ...
+// configure the provider 
+variable "region_variable" {
+    default = "us-east-1"
+}
+
+variable "instance_variable" {
+    default = "simple_web_instance"
+}
+
+provider "aws" {
+    region = var.region_variable
+} 
+
+// create a keypair 
+resource "tls_private_key" "testerman" {
+    algorithm = "RSA"
+    rsa_bits = "4096"
+}
+
+resource "aws_key_pair" "testerman_key_pair" {
+    key_name = "testermankey6.pem"
+    public_key = tls_private_key.testerman.public_key_openssh
+
+}
+
+//save the key locally 
+resource "local_file" "stored_private_key" {
+    content = tls_private_key.testerman.private_key_pem
+    filename = aws_key_pair.testerman_key_pair.key_name
+
+}
+
+// create a security group
+resource "aws_security_group" "checking_instance_sg" {
+    name = "new_instance_sec_group_6"
+    description = "Created security group with Terraform"
+    # vpc_id = "vpc-05eea395b4623bebc"
+        
+    ingress {
+        description = "Ingress for allowing traffic into the instance"
+        protocol = "TCP"
+        from_port = 22
+        to_port = 22
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+     ingress {
+        description = "Ingress for allowing traffic for HTTP"
+        protocol = "TCP"
+        from_port = 80
+        to_port = 80
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    egress {
+        description = "Egress for the instances created with Terraform"
+        from_port = "0"
+        to_port = "0"
+        protocol = "-1"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    tags = {
+        Name = "A simple security group for the instances"
+    }
+}
+
+
+// create an ec2 instance 
+resource "aws_instance" "checking_instance" {
+    ami = "ami-07d9b9ddc6cd8dd30"
+    instance_type = "t2.micro"
+    # subnet_id = "subnet-07677ad957fa848a4"
+    key_name = aws_key_pair.testerman_key_pair.key_name
+    associate_public_ip_address = true
+    vpc_security_group_ids = [aws_security_group.checking_instance_sg.id]
+    # user_data = file("_devops/setup.sh")
+    tags = {
+        Name = var.instance_variable
+    }
+
+    connection {
+        type     = "ssh"
+        user     = "ubuntu"
+        private_key = file(aws_key_pair.testerman_key_pair.key_name)
+        host     = self.public_ip
+    }
+
+    provisioner "remote-exec" {
+       script = "_setup.sh"
+       when = create
+    }
+
+}
